c97934a56b027ce639b31a59f138bb15582edec1
Jose Quiceno <josequiceno000@gmail.com>
2026-02-18 19:03:24 -0600

    Create variables for operation

--- js/script.js
+++ js/script.js
@@ -2,25 +2,9 @@ const add = function(a, b) {
     return a + b;
 }
 
-// ✓ TESTS
-// console.log(add(2, 5));
-// console.log(add(2, 5) === 7);
-// console.log(add(-1, 10));
-// console.log(add(-1, 10) === 9);
-// console.log(add(20, 400));
-// console.log(add(20, 400) === 420);
-
 const subtract = function(a, b) {
     return a - b;
 }
-
-// ✓ TESTS
-// console.log(subtract(5, 5));
-// console.log(subtract(5, 5) === 0);
-// console.log(subtract(20, 10));
-// console.log(subtract(20, 10) === 10);
-// console.log(subtract(20, 400));
-// console.log(subtract(20, 400) === -380);
 
 const multiply = function(a, b) {
     return a * b;
@@ -37,10 +21,6 @@ const divide = function(a, b) {
     }
 }
 
-// ✓ TESTS
-// console.log(divide(20, 0));
-// console.log(multiply(20, 2) === 40);
-// console.log(divide(20, 10));
-// console.log(divide(20, 10) === 2);
-// console.log(multiply(20, 400));
-// console.log(multiply(-20, 400) === -8000);
+let firstNumber;
+let operator;
+let secondNumber;
