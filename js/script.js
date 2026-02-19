f6f43e087a817b543c3c9e2764a8ad368712ad77
Jose Quiceno <josequiceno000@gmail.com>
2026-02-18 18:51:59 -0600

    Implement subract function

--- js/script.js
+++ js/script.js
@@ -9,3 +9,15 @@ const add = function(a, b) {
 // console.log(add(-1, 10) === 9);
 // console.log(add(20, 400));
 // console.log(add(20, 400) === 420);
+
+const subtract = function(a, b) {
+    return a - b;
+}
+
+// ✓ TESTS
+// console.log(subtract(5, 5));
+// console.log(subtract(5, 5) === 0);
+// console.log(subtract(20, 10));
+// console.log(subtract(20, 10) === 10);
+// console.log(subtract(20, 400));
+// console.log(subtract(20, 400) === -380);
