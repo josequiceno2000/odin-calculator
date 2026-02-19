7ba82546ebf3c01f8d691c08b136158fe624abd8
Jose Quiceno <josequiceno000@gmail.com>
2026-02-18 18:50:03 -0600

    Implement add function

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const add = function(a, b) {
+    return a + b;
+}
+
+// ✓ TESTS
+// console.log(add(2, 5));
+// console.log(add(2, 5) === 7);
+// console.log(add(-1, 10));
+// console.log(add(-1, 10) === 9);
+// console.log(add(20, 400));
+// console.log(add(20, 400) === 420);
